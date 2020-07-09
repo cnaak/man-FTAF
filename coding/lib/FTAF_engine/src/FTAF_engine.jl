4089b604f08faab0c906896c3863bb9f58968d91
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2020-07-09 11:51:35 -0300

    UPD on the interface

--- coding/lib/FTAF_engine/src/FTAF_engine.jl
+++ coding/lib/FTAF_engine/src/FTAF_engine.jl
@@ -6,22 +6,22 @@ module FTAF_engine
 #----------------------------------------------------------------------------------------------#
 
 """
-Function to return the instantaneous combustion chamber volume, in m³.
+Function to return the instantaneous combustion chamber volume, in \$m³\$.
 """
 function V end
 
 """
-Function to return the engine's total piston displaced volume, in m³.
+Function to return the engine's total piston displaced volume, in \$m³\$.
 """
 function Vd end
 
 """
-Function to return the single-cylinder piston displaced volume, in m³.
+Function to return the single-cylinder piston displaced volume, in \$m³\$.
 """
 function Vdu end
 
 """
-Function to return the single-cylinder TDC volume, in m³.
+Function to return the single-cylinder TDC volume, in \$m³\$.
 """
 function VTDC end
 
@@ -29,7 +29,7 @@ function VTDC end
 Vmin = VTDC
 
 """
-Function to return the single-cylinder BDC volume, in m³.
+Function to return the single-cylinder BDC volume, in \$m³\$.
 """
 function VBDC end
 
@@ -42,22 +42,22 @@ Function to return the engine's cylinder count.
 function z end
 
 """
-Function to return the piston diameter, in m.
+Function to return the piston diameter, in \$m\$.
 """
 function D end
 
 """
-Function to return the piston stroke, in m.
+Function to return the piston stroke, in \$m\$.
 """
 function S end
 
 """
-Function to return the connecting rod length, in m.
+Function to return the connecting rod length, in \$m\$.
 """
 function L end
 
 """
-Function to return the crank radius, in m.
+Function to return the crank radius, in \$m\$.
 """
 function R end
 
