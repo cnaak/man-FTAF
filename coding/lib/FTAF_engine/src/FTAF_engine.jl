b37dd9ca60742a1a262e0bea9cb1c0b10fe32988
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2020-07-09 11:07:35 -0300

    UPD on the interface

--- coding/lib/FTAF_engine/src/FTAF_engine.jl
+++ coding/lib/FTAF_engine/src/FTAF_engine.jl
@@ -1,6 +1,46 @@
 # Module
 module FTAF_engine
 
+#----------------------------------------------------------------------------------------------#
+#                                          Interface                                           #
+#----------------------------------------------------------------------------------------------#
+
+"""
+Function to return the instantaneous combustion chamber volume, in m³.
+"""
+function V end
+
+"""
+Function to return the engine's total piston displaced volume, in m³.
+"""
+function Vd end
+
+"""
+Function to return the single-cylinder piston displaced volume, in m³.
+"""
+function Vdu end
+
+"""
+Function to return the single-cylinder TDC volume, in m³.
+"""
+function VTDC end
+
+# VTDC's Vmin alias
+Vmin = VTDC
+
+"""
+Function to return the single-cylinder BDC volume, in m³.
+"""
+function VBDC end
+
+# VBDC's Vmin alias
+Vmax = VBDC
+
+
+"""
+Function to return the engine's cylinder count.
+"""
+function z end
 
 # Module
 end
