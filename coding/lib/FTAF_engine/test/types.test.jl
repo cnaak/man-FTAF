aacd3394acc575903c44d5756406feaecc9fdf8e
C Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2020-08-21 21:28:21 -0300

    UPD implementing tests; 298/312 tests passing

--- coding/lib/FTAF_engine/test/types.test.jl
+++ coding/lib/FTAF_engine/test/types.test.jl
@@ -35,6 +35,9 @@ end
                 @test eng.D2S(_emr, 𝔻) == 𝕤
                 @test eng.D2R(_emr, 𝔻) == 𝕣
                 @test eng.D2L(_emr, 𝔻) == 𝕝
+                @test eng.S2D(_emr, 𝕤) == 𝕤 * one(ℙ) / ℙ(_rsd)
+                @test eng.R2D(_emr, 𝕣) == 2𝕣 * one(ℙ) / ℙ(_rsd)
+                @test eng.L2D(_emr, 𝕝) == 2𝕝 * one(ℙ) / ℙ(_rlr) * one(ℙ) / ℙ(_rsd)
             end
         end
     end
